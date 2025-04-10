ec5cfae33caf0aba204c33be0e4060236cfa5894
Zumar Syed Bukhari <zumarawesome@gmail.com>
2025-04-09 21:01:44 -0600

    Adding in Taskbar Icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,7 +8,9 @@ import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { MaterialIcons } from '@expo/vector-icons';
-
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -33,16 +35,16 @@ export default function TabLayout() {
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+            <AntDesign name="home" size={24} color="white" />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="countries"
         options={{
-          title: "Explore",
+          title: "Countries",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <Ionicons name="globe-outline" size={24} color="white" />
           ),
         }}
       />
@@ -51,7 +53,7 @@ export default function TabLayout() {
         options={{
           title: "Profile",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <AntDesign name="user" size={24} color="white" />
           ),
         }}
       />
@@ -60,7 +62,7 @@ export default function TabLayout() {
         options={{
           title: "Compass",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <Ionicons name="compass" size={24} color="white" />
           ),
         }}
       />
@@ -69,7 +71,7 @@ export default function TabLayout() {
         options={{
           title: "Weekly",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <FontAwesome5 name="calendar-week" size={24} color="white" />
           ),
         }}
       />
